c2f9cfc2220c8572314fd46cb9670541d3e8a6a5
Neil <neilwl022@gmail.com>
2025-02-13 17:06:30 -0500

    openweather key

--- weather_crew_example/WeaterAPI.py
+++ weather_crew_example/WeaterAPI.py
@@ -1,11 +1,15 @@
+import os
 import requests
 from langchain_community.tools import tool
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @tool("Weather API")
 def retrieve_weather_data(latitude: str, longitude: str):
     """ Useful for retrieving weater data from an api given latitude and longitude """
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=7f2cf6040f9e28721de531468d34985e"
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={os.getenv('OPENWEATHER_API_KEY')}"
 
     response = requests.get(url)
 
